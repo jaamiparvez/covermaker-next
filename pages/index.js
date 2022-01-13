8e1a537e69ca40526a2a5e964830d3bc24c24479
jaami parvez <jaami.parvez@gmail.com>
2022-01-13 00:19:48 -0600

    import Covermaker component in Home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Covermaker from "../components/Covermaker";
+
 export default function Home() {
   return (
     <div className="container">
       <h1 className="text-4xl text-center">Covermaker</h1>
+      <Covermaker />
     </div>
   );
 }

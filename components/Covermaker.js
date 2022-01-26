075f17d36c2e1986dec8fd431ef26e1205b3ee5f
jaami parvez <jaami.parvez@gmail.com>
2022-01-26 15:09:30 -0600

    Add FabricCanvas and Properties components

--- components/Covermaker.js
+++ components/Covermaker.js
@@ -1,9 +1,20 @@
+import FabricCanvas from "./FabricCanvas";
+import Properties from "./Properties";
+import { FabricContextProvider } from "../context/FabricContext";
 export default function Covermaker() {
   return (
-    <div>
-      {/* canvas */}
-
-      {/* properties panel */}
-    </div>
+    <FabricContextProvider>
+      <div className="container">
+        <h1>Cover Maker</h1>
+        <div className="flex">
+          <div className="col">
+            <FabricCanvas />
+          </div>
+          <div className="col">
+            <Properties />
+          </div>
+        </div>
+      </div>
+    </FabricContextProvider>
   );
 }

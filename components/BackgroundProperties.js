8753459b50029d2bea03ee188a4a8b1770d363ed
jaami parvez <jaami.parvez@gmail.com>
2022-03-13 20:08:34 -0600

    Add styles for BackgroundProperties component

--- components/BackgroundProperties.js
+++ components/BackgroundProperties.js
@@ -17,50 +17,56 @@ const BackgroundProperties = () => {
 
   return (
     <div>
-      <div className="row g-0">Type</div>
+      <div className="mb-4">Type</div>
       <div
-        className="btn-group"
+        className="flex flex-row rounded-md mb-8"
         role="group"
-        aria-label="Basic radio toggle button group"
+      
       >
+        <div className="">
         <input
           type="radio"
-          className="btn-check"
+          className="sr-only peer focus:outline-none focus:ring focus:ring-violet-300"
           name="btnradio"
           id="Solid"
           autoComplete="off"
           onChange={changeType}
           checked={type === "Solid" ? true : false}
         />
-        <label className="btn btn-outline-secondary" htmlFor="Solid">
+        <label className="peer-checked:bg-gray-500 peer-checked:text-white bg-white text-gray-500 hover:bg-gray-500 hover:text-white 
+        border border-r-0 border-gray-500 rounded-l-md px-4 py-2 mx-0 focus:outline-none focus:ring focus:ring-violet-300 " htmlFor="Solid">
           Solid
         </label>
-
+        </div>
+        <div>
         <input
           type="radio"
-          className="btn-check"
+          className="sr-only peer"
           name="btnradio"
           id="Gradient"
           autoComplete="off"
           onChange={changeType}
           checked={type === "Gradient" ? true : false}
         />
-        <label className="btn btn-outline-secondary" htmlFor="Gradient">
+        <label className="peer-checked:bg-gray-500 peer-checked:text-white bg-white text-gray-500 hover:bg-gray-500 hover:text-white
+         border border-gray-500  px-4 py-2 mx-0 outline-none focus:ring" htmlFor="Gradient">
           Gradient
         </label>
-
+        </div>
+        <div>
         <input
           type="radio"
-          className="btn-check"
+          className="sr-only peer"
           name="btnradio"
           id="Image"
           autoComplete="off"
           onChange={changeType}
           checked={type === "Image" ? true : false}
         />
-        <label className="btn btn-outline-secondary" htmlFor="Image">
+        <label className="peer-checked:bg-gray-500 peer-checked:text-white bg-white text-gray-500 hover:bg-gray-500 hover:text-white border border-l-0 border-gray-500 rounded-r-md px-4 py-2 mx-0  focus:ring" htmlFor="Image">
           Image
         </label>
+        </div>
       </div>
       <div className="row g-0">
         {type === "Solid" && <SolidBackground />}

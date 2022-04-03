395cffadd6eace45a8cff9124b63b4aba1723a39
jaami parvez <jaami.parvez@gmail.com>
2022-04-03 16:48:42 -0600

    Add svg for x icon

--- components/TextBackgroundColor.js
+++ components/TextBackgroundColor.js
@@ -56,9 +56,25 @@ const TextBackgroundColor = () => {
         {activeObject && activeObject.textBackgroundColor && (
           <button
             onClick={removeColor}
-            className="btn-close"
+            className="pl-2 text-gray-500 hover:text-gray-600"
             aria-label="Close"
-          ></button>
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              class=""
+              width="20"
+              height="20"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 16 16"
+            >
+              <g fill="currentColor" fill-rule="evenodd">
+                <path d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"></path>
+                <path d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"></path>
+              </g>
+            </svg>
+          </button>
         )}
       </div>
     </div>
